72287f6819388e28e2840987f3597df13ddcca68
阿星不是程序员 <1031900093@qq.com>
2026-08-13 14:46:11 +0800

    去掉无用的内容

--- sql/hmdp_0.sql
+++ sql/hmdp_0.sql
@@ -1,8 +1,7 @@
 USE hmdp_0;
 
 DROP TABLE IF EXISTS `tb_blog`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `tb_blog` (
                            `id` bigint unsigned NOT NULL COMMENT '主键',
                            `shop_id` bigint NOT NULL COMMENT '商户id',
@@ -702,28 +701,9 @@ CREATE TABLE `tb_voucher_reconcile_log_1` (
                                               KEY `idx_message_id` (`message_id`) USING BTREE,
                                               KEY `idx_trace_id` (`trace_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tb_voucher_reconcile_log_1`
---
 
 LOCK TABLES `tb_voucher_reconcile_log_1` WRITE;
-/*!40000 ALTER TABLE `tb_voucher_reconcile_log_1` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_voucher_reconcile_log_1` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Dumping routines for database 'hmdp_0'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-11-24 10:08:40
--- sql/hmdp_1.sql
+++ sql/hmdp_1.sql
@@ -1,8 +1,7 @@
 USE hmdp_1;
 
 DROP TABLE IF EXISTS `tb_blog`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `tb_blog` (
                            `id` bigint unsigned NOT NULL COMMENT '主键',
                            `shop_id` bigint NOT NULL COMMENT '商户id',
@@ -714,18 +713,3 @@ LOCK TABLES `tb_voucher_reconcile_log_1` WRITE;
 /*!40000 ALTER TABLE `tb_voucher_reconcile_log_1` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_voucher_reconcile_log_1` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'hmdp_1'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-11-24 10:09:29
